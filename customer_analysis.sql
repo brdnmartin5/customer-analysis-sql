0430064bd61cb8997f5574886382f3cf3b27f7ae
Brandon Martin <brdnmartin5@gmail.com>
2026-05-26 13:43:37 -0700

    Add files via upload

---
+++ customer_analysis.sql
@@ -0,0 +1,40 @@
+USE practice_db;
+DROP TABLE IF EXISTS customers;
+
+CREATE TABLE customers (
+	CustomerID INT,
+    Gender VARCHAR(10),
+    Age INT, 
+    Annual_Income INT,
+    Spending_Score INT
+);
+
+INSERT INTO customers VALUES
+(1,'Male',19,15,39),
+(2,'Male',21,15,81),
+(3,'Female',20,16,6),
+(4,'Female',23,16,77),
+(5,'Female',31,17,40),
+(6,'Female',22,17,76),
+(7,'Female',35,18,6),
+(8,'Female',23,18,94),
+(9,'Male',64,19,3),
+(10,'Female',30,19,72),
+(11,'Male',67,20,14),
+(12,'Female',35,20,99),
+(13,'Female',58,20,15),
+(14,'Female',24,20,77),
+(15,'Male',37,20,13);
+
+
+SELECT 
+	CASE 
+    WHEN Age < 25 THEN 'Under 25'
+    WHEN Age BETWEEN 25 AND 40 THEN '25-40'
+    ELSE '40+'
+END AS Age_Group,
+Gender,
+AVG(Spending_Score) AS Avg_Spending
+FROM customers
+GROUP BY Gender, Age_Group
+    
